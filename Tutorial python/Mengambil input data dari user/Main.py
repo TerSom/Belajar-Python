c8c59239daadbbfd7d03181b25091a341578d2a9
TerSom <140949512+TerSom@users.noreply.github.com>
2025-05-30 19:48:57 +0700

    Add files via upload

---
+++ Tutorial python/Mengambil input data dari user/Main.py
@@ -0,0 +1,11 @@
+data = input("masukin data :")
+print("data adalah :", data , type(data))
+
+Number = int(input("masukin Number :"))
+print("data adalah :", Number , type(Number))
+
+Float = float(input("masukin Float :"))
+print("data adalah :", Float , type(Float))
+
+Bool = bool(int(input("masukan boolean : ")))
+print("data adalah :", Bool , type(Bool))
