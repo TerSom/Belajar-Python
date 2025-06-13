229c16d53ec8faaf6d6e073a1c95c5fc5a9de2fa
TerSom <140949512+TerSom@users.noreply.github.com>
2025-06-13 14:45:28 +0700

    Add files via upload

---
+++ Tutorial python/14 - Operator Assignment/main.py
@@ -0,0 +1,50 @@
+# assiignment airtmatika
+
+a = 5
+print(a)
+a += 2
+print("nilai a adalah",a)
+a -= 2
+print("nilai a adalah",a)
+a *= 10
+print("nilai a adalah",a)
+a /= 2 
+print("nilai a adalah",a)
+
+# operasi modulus
+a = 10
+a %= 3
+print("nilai a adalah",a)
+
+# operasi exponen
+a = 10
+a **= 2
+print("nilai a adalah",a)
+
+# operasi bitwise
+# OR
+c = False
+c |= False
+print("hasilnya adalah",c)
+c |= True
+print("hasilnya adalah",c)
+
+# AND
+c &= True
+print("hasilnya adalah",c)
+
+c &= False
+print("hasilnya adalah",c)
+
+# XOR
+c = True
+c ^= False
+print("hasilnya",c)
+c ^= True
+print("hasilnya",c)
+
+
+a = 10
+a += 10
+a = a + 10
+print(a)
