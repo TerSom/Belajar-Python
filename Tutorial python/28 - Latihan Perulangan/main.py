5725f60ef86c486330c6283a273ff0ef197d8266
TerSom <140949512+TerSom@users.noreply.github.com>
2025-06-18 15:24:09 +0700

    Add files via upload

---
+++ Tutorial python/28 - Latihan Perulangan/main.py
@@ -0,0 +1,20 @@
+# latihan pengulangan membuat segitiga
+
+# pakai for
+
+sisi = 5
+
+count = 1
+for i in range(sisi):
+    print("*"*count )
+    count += 1
+
+# pakai while
+
+count = 1
+while True:
+    print("*"*count)
+    count += 1
+    if count > sisi:
+        break
+    
