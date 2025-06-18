e0865a54910d089f6674e0ea407f54b61819a4d8
TerSom <140949512+TerSom@users.noreply.github.com>
2025-06-18 09:13:33 +0700

    Add files via upload

---
+++ Tutorial python/25 - While Loop (Perulangan)/main.py
@@ -0,0 +1,18 @@
+# pengulangan while lopp
+
+# while kondisi:
+#     aksi satu 
+#     aksi dua
+
+# akhir dari pemrograman
+
+angka = 0
+print(f"angka ke {angka}")
+
+while angka < 10:
+    angka += 1
+    print(f"gg gmaing angka ke {angka}")
+    print("umay")
+
+print("stop")
+
