8377fcd8b0ef2d7fafc07959c096a4f9c93ec3a9
TerSom <140949512+TerSom@users.noreply.github.com>
2025-06-18 08:56:10 +0700

    Add files via upload

---
+++ Tutorial python/24 - For Loop (Perulangan)/main.py
@@ -0,0 +1,47 @@
+# belajar pengulangan atau loop
+
+# for kondisi:
+#   aksi
+
+# dengan loop list
+
+angkaList = [1,2,3,4,5,6]
+
+for i in angkaList:
+    print(f"umay somay yang ke {i}")
+
+
+print(f"\n ======= \n")
+
+angkaList = [1,6,8,23,5,6]
+
+for i in angkaList:
+    print(f"umay somay yang ke {i}")
+
+print(f"\n ======= \n")
+
+# looping menggukan range 
+
+angkaRange = range(10)
+
+for i in angkaRange:
+    print(f"umay somay ke {i}")
+
+print(f"\n ======= \n")
+
+angkaRange = range(1,10)
+
+for i in angkaRange:
+    print(f"umay somay ke {i}")
+
+for i in range(12):
+    print(f"umay somay ke {i}")
+
+print(f"\n ======= \n")
+
+# looping string
+
+nama = "umay somay gg gaming"
+
+for i in nama:
+    print(i)
