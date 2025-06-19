a4bba3feadcb240ad8850224ad0495f84ccaf5b2
TerSom <140949512+TerSom@users.noreply.github.com>
2025-06-19 09:30:37 +0700

    Add files via upload

---
+++ Tutorial python/- 29 - List/main.py
@@ -0,0 +1,57 @@
+# belajar list
+
+# kumpulan data list number/int
+
+angka = [1,2,3,4]
+print(angka)
+angka = [1,6856,4,7]
+print(angka)
+
+# kumpulan data list string
+
+nama = ["umay","somay","mieayam"]
+print(nama)
+
+# kumpulan data list boolean
+
+bool = [True,False,False,True]
+print(bool)
+
+# kumpulan data list campuran
+
+campuran = [1,"umay",True,1,"umay",True]
+print(campuran)
+
+# membuat alternatif list menggunakan range
+dataRange = range(0,10,2) #range(start,stop,step)
+print(dataRange)
+dataListRange = list(dataRange)
+print(dataListRange)
+
+# membuat list pake for loop
+listForLoop = [i for i in range(0,10)]
+print(listForLoop)
+
+# membuat list pake for dan if
+
+listForIf = [i for i in range(0,10) if i != 5]
+print(listForIf)
+
+listForIf = [i for i in range(0,10) if i%2 == 0]
+print(listForIf)
+
+listForIf = [i for i in range(0,10) if i%2 != 0]
+print(listForIf)
+
+
+
+
+
+
+
+
+
+
+
+
+
