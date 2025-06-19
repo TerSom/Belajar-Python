1f9e0005a527a248c79188d1b9e3de96ce964c51
TerSom <140949512+TerSom@users.noreply.github.com>
2025-06-19 08:59:17 +0700

    Update main.py

--- Tutorial python/28 - Latihan Perulangan/main.py
+++ Tutorial python/28 - Latihan Perulangan/main.py
@@ -1,20 +1,68 @@
-# latihan pengulangan membuat segitiga
+count = 0
+sisi = 10
 
-# pakai for
-
-sisi = 5
-
-count = 1
+# # menggunakan for
 for i in range(sisi):
-    print("*"*count )
     count += 1
+    print("*"*count)
 
-# pakai while
 
+# menggunakan while
+count = 0
+while True:
+    count += 1 
+    print("*"*count)
+    if count == 5:
+        break
+
+# menggunakan while genap saja
 count = 1
 while True:
+    if count%2:
+        count += 1
+        continue
     print("*"*count)
     count += 1
     if count > sisi:
         break
+
+# menggunakan while ganjil saja
+count = 1
+while True:
+    if count%2:
+        print("*"*count)
+        count += 1
+    else:
+        count += 1
+        continue
     
+    if count > sisi:
+        break
+
+
+# menggunakan while ganjil membuat segitiga
+count = 1
+spasi = int(sisi/2)
+
+while True:
+    if count%2:
+        print(" "*spasi,"*"*count)
+        spasi -= 1
+        count += 1
+    else:
+        count += 1
+        continue
+    if count > sisi:
+        break
+
+# # membuat segitiga
+
+count = 1
+spasi = int(sisi/2)
+
+while True:
+    print(" "*spasi,"*"*count)
+    spasi -= 1
+    count += 2
+    if count > sisi:
+        break
