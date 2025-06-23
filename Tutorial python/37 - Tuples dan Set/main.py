aeed57f8379e06cabb4a462dca044478ae34f4ca
TerSom <140949512+TerSom@users.noreply.github.com>
2025-06-23 09:45:40 +0700

    Add files via upload

---
+++ Tutorial python/37 - Tuples dan Set/main.py
@@ -0,0 +1,19 @@
+# data tuples dan set
+
+# data list
+angkaRandom = [1,2,5,3,5,3,234,24,5,646]
+print(angkaRandom)
+# bisa di ganti angkanya bisa di appen dll
+
+# data tuples
+angkaRandom = (1,5,4,34,4,4,3,4,5,6,7,3,4,5)
+print(angkaRandom)
+# angkaRandom[1] = 23
+# angkaRandom.append(1)
+# data tuples tidak bisa di tambah atau di appen di tetap atau constanta
+
+# data set
+angkaRandom = {2,34,5,6,8,90,4}
+# angkaRandom[0]
+print(angkaRandom)
+# data set tidak ada indexnya tapi bisa di tambah dll
