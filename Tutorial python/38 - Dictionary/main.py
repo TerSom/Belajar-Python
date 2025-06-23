889f78488c2ae8224d6d26dafac0e604a0fe515d
TerSom <140949512+TerSom@users.noreply.github.com>
2025-06-23 09:57:22 +0700

    Add files via upload

---
+++ Tutorial python/38 - Dictionary/main.py
@@ -0,0 +1,12 @@
+datList = ["kocak","kocakas"]
+
+
+dataDictionaty = {
+    'key1' : "darmaan",
+    'key2' : 324242,
+    'key3' : "darmadasdan",
+    'key4' : "darmaanasda",
+    'key5' : datList,
+}
+
+print(dataDictionaty["key5"])
