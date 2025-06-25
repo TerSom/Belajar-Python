d35d0de4c98e4165b3ad6c1fe736342be9f68b42
TerSom <140949512+TerSom@users.noreply.github.com>
2025-06-25 08:42:11 +0700

    Add files via upload

---
+++ Tutorial python/44 - Pengenalan Fungsi/main.py
@@ -0,0 +1,17 @@
+# pengenalan fungsi
+
+def Hello():
+    print("ini adalah fungsi Hello")
+    print("Selamat Datang di Pengenalan Fungsi")
+
+Hello()
+Hello()
+
+# Hello2()
+
+def Hello2():
+    print("ini adalah fungsi Hello2")
+    print("ini adalah fungsi Hello2")
+
+
+Hello2()
