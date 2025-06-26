96e8e39cbb4bad584c1391b94782ee217c96e5a4
TerSom <140949512+TerSom@users.noreply.github.com>
2025-06-26 15:25:44 +0700

    Add files via upload

---
+++ Tutorial python/57 - __Init__.py pada Package/main.py
@@ -0,0 +1,15 @@
+import sains
+import sains.matematika
+
+gaya = sains.fisika.gaya(10,20)
+print(f"gaya nya adalaah {gaya}")
+
+tambah = sains.matematika.basic.tambah(10,10,10)
+print(f"hasil dari tambah {tambah} ")
+
+kali = sains.matematika.basic.kali(10,10,10)
+print(f"hasil dari kali {kali} ")
+
+pangkat = sains.matematika.scientific.pangkat(3)
+print(f"hasil dari pangkat {pangkat(5)} ")
+
